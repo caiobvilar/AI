692259ff5662990512e6ca7cfe9386642eabd9d3
Caio J. B. V. Guimaraes <caio.b.vilar@gmail.com>
2019-02-05 12:06:25 -0500

    update

--- Neural_Networks/BM/main.cpp
+++ Neural_Networks/BM/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+
+struct Hidden			//Hidden units
+{
+	std::vector<double> hh_weights; //hidden-hidden interaction weights
+	std::vector<double> hv_weights; //hidden-visible interaction weights
+	uint8_t state;									//Hidden units state
+};
+
+struct Visible		//Visible units
+{
+	std::vector<double> vv_weights; //visible-visible interaction weights
+	std::vector<double> vh_weights; //visible-hidden interaction weights
+	uint8_t state;									//visible unit states
+};
 
 
 const int node_count = 3;
 
-
+const AND_States[4][4];
 
 int main(int argc, char *argv[])
 {
-	double J[node_count][node_count]; //weight matrix
-	double h[node_count];							//Bias vector;
-	uint8_t states[node_count];
 	
 
 	

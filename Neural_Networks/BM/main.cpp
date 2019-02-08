74cdc5795a9c4a686e878bff0e059874e6f43f68
Caio J. B. V. Guimaraes <caio.b.vilar@gmail.com>
2019-02-08 16:35:05 -0500

    update

--- Neural_Networks/BM/main.cpp
+++ Neural_Networks/BM/main.cpp
@@ -3,11 +3,12 @@
 // If I have 2 visible units, say A and B, A(0,0) and B(0,1)
 int V[1][3] = {1,1,0}; //which means first two units are visible and the last isnt.
 
-double B[rows1][cols1] = {1,1,-2};
-double S1[rows1][cols1] = {0,0,1};
-double S2[rows1][cols1] = {0,1,1};
-double S3[rows1][cols1] = {1,0,1};
-double S4[rows1][cols1] = {1,1,0};
+double B[rows1][cols1] = {1,1,-2};										//Bias vector
+double S1[rows1][cols1] = {0,0,1};										//Training set 1
+double S2[rows1][cols1] = {0,1,1};										//Training set 2
+double S3[rows1][cols1] = {1,0,1};										//Training set 3
+double S4[rows1][cols1] = {1,1,0};										//Training set 4
+double S_AND[SAMPLES][NODES]
 double W[rows2][cols2] = {{0,-1,2},{-1,0,2},{2,2,0}};
 double T[rows1][cols2] = {0,0,0};
 double S[rows1][cols1];
@@ -25,30 +26,71 @@ int main(int argc, char *argv[])
 	{
 		case 1:
 			array_copy(S1[0],S[0],rows1,cols1);
+			std::cout << "[Initial States]" << std::endl;
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Initial Weights]" << std::endl;
+			show_values(W[0],rows2,cols2);
+			std::cout << "[Finding local field]" << std::endl;
+			multiply_matrices(S[0],W[0],T[0],rows1,cols1,cols2);
+			show_values(T[0],rows1,cols2);
+			std::cout << "[Adding Bias]" << std::endl;
+			add_bias(B[0],T[0],rows1,cols1);
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Evaluating States]" << std::endl;
+			clamped_phase(S[0],T[0],rows1,cols1,B[0],V[0]);
+			show_values(S[0],rows1,cols1);
 			break;
 		case 2:
 			array_copy(S2[0],S[0],rows1,cols1);
+			std::cout << "[Initial States]" << std::endl;
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Initial Weights]" << std::endl;
+			show_values(W[0],rows2,cols2);
+			std::cout << "[Finding local field]" << std::endl;
+			multiply_matrices(S[0],W[0],T[0],rows1,cols1,cols2);
+			show_values(T[0],rows1,cols2);
+			std::cout << "[Adding Bias]" << std::endl;
+			add_bias(B[0],T[0],rows1,cols1);
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Evaluating States]" << std::endl;
+			clamped_phase(S[0],T[0],rows1,cols1,B[0],V[0]);
+			show_values(S[0],rows1,cols1);
 			break;
 		case 3:
 			array_copy(S3[0],S[0],rows1,cols1);
+			std::cout << "[Initial States]" << std::endl;
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Initial Weights]" << std::endl;
+			show_values(W[0],rows2,cols2);
+			std::cout << "[Finding local field]" << std::endl;
+			multiply_matrices(S[0],W[0],T[0],rows1,cols1,cols2);
+			show_values(T[0],rows1,cols2);
+			std::cout << "[Adding Bias]" << std::endl;
+			add_bias(B[0],T[0],rows1,cols1);
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Evaluating States]" << std::endl;
+			clamped_phase(S[0],T[0],rows1,cols1,B[0],V[0]);
+			show_values(S[0],rows1,cols1);
 			break;
 		case 4:
 			array_copy(S4[0],S[0],rows1,cols1);
+			std::cout << "[Initial States]" << std::endl;
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Initial Weights]" << std::endl;
+			show_values(W[0],rows2,cols2);
+			std::cout << "[Finding local field]" << std::endl;
+			multiply_matrices(S[0],W[0],T[0],rows1,cols1,cols2);
+			show_values(T[0],rows1,cols2);
+			std::cout << "[Adding Bias]" << std::endl;
+			add_bias(B[0],T[0],rows1,cols1);
+			show_values(S[0],rows1,cols1);
+			std::cout << "[Evaluating States]" << std::endl;
+			clamped_phase(S[0],T[0],rows1,cols1,B[0],V[0]);
+			show_values(S[0],rows1,cols1);
+			break;
+		case 5: //Training case.
 			break;
 	}
-	std::cout << "[Initial States]" << std::endl;
-	show_values(S[0],rows1,cols1);
-	std::cout << "[Initial Weights]" << std::endl;
-	show_values(W[0],rows2,cols2);
-	std::cout << "[Finding local field]" << std::endl;
-	multiply_matrices(S[0],W[0],T[0],rows1,cols1,cols2);
-	show_values(T[0],rows1,cols2);
-	std::cout << "[Adding Bias]" << std::endl;
-	add_bias(B[0],T[0],rows1,cols1);
-	show_values(S[0],rows1,cols1);
-	std::cout << "[Evaluating States]" << std::endl;
-	clamped_phase(S[0],T[0],rows1,cols1,B[0],V[0]);
-	show_values(S[0],rows1,cols1);
 	return 0;
 }
 
